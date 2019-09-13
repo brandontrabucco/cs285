ee9528a82ddefbb01e575a9a625c6e59d1052be4
Brandon Trabucco <brandon@btrabucco.com>
2019-09-13 14:59:49 -0700

    updated launmch script

--- hw1/launch.sh
+++ hw1/launch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-bash $1/ant/launch.sh $1
-bash $1/half_cheetah/launch.sh $1
-bash $1/hopper/launch.sh $1
-bash $1/humanoid/launch.sh $1
-bash $1/walker2d/launch.sh $1
+bash $1/hw1/ant/launch.sh $1
+bash $1/hw1/half_cheetah/launch.sh $1
+bash $1/hw1/hopper/launch.sh $1
+bash $1/hw1/humanoid/launch.sh $1
+bash $1/hw1/walker2d/launch.sh $1
