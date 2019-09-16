774f07e35ca7e967d6bdc65b63ed258f609a61ad
Brandon Trabucco <brandon@btrabucco.com>
2019-09-15 18:36:09 -0700

    working on eval

--- cs285/core/eval_policy.py
+++ cs285/core/eval_policy.py
@@ -5,6 +5,9 @@ from cs285.data.envs.normalized_env import NormalizedEnv
 from cs285.distributions.gaussian import Gaussian
 from cs285.data.samplers.parallel_sampler import ParallelSampler
 import tensorflow as tf
+import threading
+import copy
+import numpy as np
 
 
 eval_policy_variant = dict(
@@ -16,40 +19,74 @@ eval_policy_variant = dict(
 
 
 def eval_policy(
-    variant,
-    env_class,
-    observation_key="observation",
-    **env_kwargs
+        variant,
+        env_class,
+        observation_key="observation",
+        **env_kwargs
 ):
 
     for gpu in tf.config.experimental.list_physical_devices('GPU'):
         tf.config.experimental.set_memory_growth(gpu, True)
 
-    def make_env():
-        return NormalizedEnv(env_class, **env_kwargs)
+    all_policy_ckpts = tf.io.gfile.glob(variant["policy_ckpt"])
 
-    policy_ckpt = variant["policy_ckpt"]
+    def inner_eval_policy(
+            result_list,
+            inner_variant,
+            inner_env_class,
+            inner_observation_key="observation",
+            **inner_env_kwargs
+    ):
 
-    def make_policy():
-        return Gaussian(
-            tf.keras.models.load_model(
-                policy_ckpt,
-                compile=False),
-            std=variant["exploration_noise_std"])
+        def make_env():
+            return NormalizedEnv(inner_env_class, **inner_env_kwargs)
 
-    policy = make_policy()
+        def make_policy():
+            return Gaussian(
+                tf.keras.models.load_model(
+                    inner_variant["policy_ckpt"],
+                    compile=False),
+                std=inner_variant["exploration_noise_std"])
 
-    def selector(x):
-        return x[observation_key]
+        policy = make_policy()
 
-    sampler = ParallelSampler(
-        make_env,
-        make_policy,
-        policy,
-        num_threads=variant["num_threads"],
-        max_path_length=variant["max_path_length"],
-        selector=selector)
+        def selector(x):
+            return x[inner_observation_key]
 
-    return sampler.collect(
-        variant["num_episodes"],
-        evaluate=True)
+        sampler = ParallelSampler(
+            make_env,
+            make_policy,
+            policy,
+            num_threads=inner_variant["num_threads"],
+            max_path_length=inner_variant["max_path_length"],
+            selector=selector)
+
+        paths, mean_return, steps = sampler.collect(
+            inner_variant["num_episodes"],
+            evaluate=True)
+
+        result_list.append(mean_return)
+
+    results = []
+    threads = []
+    for policy_ckpt in all_policy_ckpts:
+        variant = copy.deepcopy(variant)
+        variant["policy_ckpt"] = policy_ckpt
+
+        threads.append(
+            threading.Thread(
+                target=inner_eval_policy,
+                args=(
+                    results,
+                    variant,
+                    env_class),
+                kwargs=dict(
+                    observation_key=observation_key,
+                    **env_kwargs)))
+
+    for t in threads:
+        t.start()
+    for t in threads:
+        t.join()
+
+    return np.mean(results), np.std(results)
--- hw1/walker2d/eval.py
+++ hw1/walker2d/eval.py
@@ -4,30 +4,23 @@
 from cs285.core.eval_policy import eval_policy, eval_policy_variant
 from gym.envs.mujoco.walker2d import Walker2dEnv
 import argparse
-import numpy as np
 
 
-def run_experiment(policy_ckpts):
+def run_experiment(policy_ckpt):
 
-    mean_returns = []
-    for policy_ckpt in policy_ckpts:
+    eval_policy_variant["exploration_noise_std"] = 0.1
+    eval_policy_variant["policy_ckpt"] = policy_ckpt
+    eval_policy_variant["num_threads"] = 10
+    eval_policy_variant["max_path_length"] = 1000
+    eval_policy_variant["num_episodes"] = 10
 
-        eval_policy_variant["exploration_noise_std"] = 0.1
-        eval_policy_variant["policy_ckpt"] = policy_ckpt
-        eval_policy_variant["num_threads"] = 10
-        eval_policy_variant["max_path_length"] = 1000
-        eval_policy_variant["num_episodes"] = 10
-
-        paths, mean_return, steps = eval_policy(eval_policy_variant, Walker2dEnv)
-        mean_returns.append(mean_return)
-
-    print("mean: {}    std: {}".format(
-        np.mean(mean_returns), np.std(mean_returns)))
+    mean, std = eval_policy(eval_policy_variant, Walker2dEnv)
+    print("mean: {}    std: {}".format(mean, std))
 
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--policy_ckpts", type=str, nargs="+")
+    parser.add_argument("--policy_ckpt", type=str)
     args = parser.parse_args()
     run_experiment(args.policy_ckpts)
