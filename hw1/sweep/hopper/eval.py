33c34e78d29c66e395c88ebb299e306ebb083ef8
Brandon Trabucco <brandon@btrabucco.com>
2019-09-16 16:25:29 -0700

    added plot generator script

--- hw1/sweep/hopper/eval.py
+++ hw1/sweep/hopper/eval.py
@@ -4,23 +4,43 @@
 from cs285.core.eval_policy import eval_policy, eval_policy_variant
 from gym.envs.mujoco.hopper import HopperEnv
 import argparse
+import matplotlib.pyplot as plt
+import copy
+import os
 
 
-def run_experiment(policy_ckpt):
+def run_experiment(data_dir):
+    mean_returns = []
+    for num_episodes in [2, 4, 6, 8, 10]:
+        variant = copy.deepcopy(eval_policy_variant)
 
-    eval_policy_variant["exploration_noise_std"] = 0.1
-    eval_policy_variant["policy_ckpt"] = policy_ckpt
-    eval_policy_variant["num_threads"] = 10
-    eval_policy_variant["max_path_length"] = 1000
-    eval_policy_variant["num_episodes"] = 10
+        variant["policy_ckpt"] = os.path.join(
+            data_dir,
+            "{}_episodes/policy.ckpt".format(num_episodes))
 
-    mean, std = eval_policy(eval_policy_variant, HopperEnv)
-    print("mean: {} std: {}".format(mean, std))
+        variant["exploration_noise_std"] = 0.1
+        variant["num_threads"] = 10
+        variant["max_path_length"] = 1000
+        variant["num_episodes"] = 10
+
+        mean, std = eval_policy(variant, HopperEnv)
+        mean_returns.append(mean)
+
+    plt.clf()
+    plt.plot(
+        [2000, 4000, 6000, 8000, 10000],
+        mean_returns)
+
+    plt.title("More Demonstrations Improves Performance")
+    plt.xlabel("Number Of Demonstrations")
+    plt.ylabel("Return Mean")
+    plt.savefig(os.path.join(
+        data_dir, "sweep_mean_return.png"))
 
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--policy_ckpt", type=str)
+    parser.add_argument("--data_dir", type=str)
     args = parser.parse_args()
-    run_experiment(args.policy_ckpt)
+    run_experiment(args.data_dir)
